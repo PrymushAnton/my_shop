b33870bbac55d2ad16cf6e98a9609bf1853a497f
Anton Prymush <anton.primush123663@gmail.com>
2023-10-16 21:19:08 +0300

    meeting 16.10.2023

--- Order/Shopping_cart_page/models.py
+++ Order/Shopping_cart_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# class Product(models.Model):
+#     pass
+
+class Cart(models.Model):
+    user = models.CharField(max_length=255)
+    # product_list = models.ManyToManyField(Product, blank=True, null=True)
