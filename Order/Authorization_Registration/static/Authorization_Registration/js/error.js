c0495ca9e401dd5a0121daf21e58a09aea654c98
Andrii Druzhha <xgodkeyx@gmail.com>
2023-11-21 23:42:36 +0200

    Fix error look

---
+++ Order/Authorization_Registration/static/Authorization_Registration/js/error.js
@@ -0,0 +1,29 @@
+let errors = document.querySelector(".errors")
+let error = document.querySelector(".error")
+
+error.style.color = "black"
+error.style.fontSize = "2em";
+error.style.top = "85%"
+error.style.left = "100%"
+// error.style.right = "0"
+error.style.whiteSpace = "nowrap"
+let x = error.getBoundingClientRect().left
+let weld = error.getBoundingClientRect().left - error.getBoundingClientRect().width
+console.log(x)
+let gate = 30
+setInterval(nado => {
+    if (x >= weld){
+        x -= gate
+        error.style.left = `${x}px`
+    } else {
+        clearInterval()
+    }
+    if (gate >= 20){
+        gate -= 2
+    } else if (gate >= 10){
+        gate -= 1.5
+    } else if (gate >= 3){
+        gate -= 1
+    }
+}, 10)
+console.log(x)
