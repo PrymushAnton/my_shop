840db58b380d954b8c017e77a8f9c179bc9a5bf6
Prymush Anton <102522374+PrymushAnton@users.noreply.github.com>
2023-11-27 21:38:03 +0200

    27.11.2023 meeting

--- Order/Authorization_Registration/static/Authorization_Registration/js/error.js
+++ Order/Authorization_Registration/static/Authorization_Registration/js/error.js
@@ -3,16 +3,14 @@ let error = document.querySelector(".error")
 
 error.style.color = "red"
 error.style.fontSize = "2em";
-error.style.top = "85%"
 error.style.left = "100%"
-// error.style.right = "0"
 error.style.whiteSpace = "nowrap"
 let x = error.getBoundingClientRect().left
 let weld = error.getBoundingClientRect().left - error.getBoundingClientRect().width
 console.log(x)
 let gate = 30
 setInterval(nado => {
-    if (x >= weld){
+    if (x >= weld - 50){
         x -= gate
         error.style.left = `${x}px`
     } else {
